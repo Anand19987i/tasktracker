8354ed54164c8a1709fc87093467581d36192fa9
Anand19987i <anandpandey1765@gmail.com>
2025-04-26 16:21:13 +0530

    first commit

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user"
-export const TASK_API_END_POINT = "http://localhost:3000/api/v1/task"
-export const PROJECT_API_END_POINT = "http://localhost:3000/api/v1/project"
+export const USER_API_END_POINT = "https://taskbackend-v63y.onrender.com/api/v1/user"
+export const TASK_API_END_POINT = "https://taskbackend-v63y.onrender.com/api/v1/task"
+export const PROJECT_API_END_POINT = "https://taskbackend-v63y.onrender.com/api/v1/project"
+
+
+// export const USER_API_END_POINT = "http://localhost:3000/api/v1/user"
+// export const TASK_API_END_POINT = "http://localhost:3000/api/v1/task"
+// export const PROJECT_API_END_POINT = "http://localhost:3000/api/v1/project"
